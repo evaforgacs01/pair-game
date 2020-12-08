094c256f7d99633d5c9000f59d04dd9471e4450e
Eve <evaforgacs@hotmail.com>
2020-12-08 17:16:24 +0100

    commit

--- pairs.js
+++ pairs.js
@@ -1,7 +1,10 @@
 
 'use strict';
 
-const cards = document.querySelectorAll('.cards');
+  let points = 0;
+
+
+const cards = document.querySelectorAll('.memory-cards');
 
   let hasFlippedCard = false;
   let lockBoard = false;
@@ -66,4 +69,52 @@ const cards = document.querySelectorAll('.cards');
   cards.forEach(card => card.addEventListener('click', flipCard));
 
 
+//  // Stopperóra.
  
+// let stopperTime = 0;
+// let stopperIsRunning = false;
+// setInterval( () => {
+//     if (!stopperIsRunning) {
+//         return;
+//     }
+    
+//     stopperTime++;
+//     const seconds = padNumbers(stopperTime % 60);
+//     const minutes = padNumbers(Math.floor(stopperTime / 60) % 60);
+//     const hours = padNumbers(Math.floor(stopperTime / 3600));
+//     const time = `${[hours, minutes, seconds].join(':')}`;
+//     const stopperFace = document.querySelector('.stopper-face');
+//     stopperFace.textContent = time;
+// }, 1000);
+
+// document.querySelector('.start-stop-btn').addEventListener('click', () => {
+//     if (stopperIsRunning) {
+//         stopperIsRunning = false;
+//         stopperTime = 0;
+//     } else {
+//         stopperIsRunning = true;
+//     }
+
+  //   const showPoints = (points) => {
+  //     document.querySelector('.user-points').textContent = points;
+  // }
+
+  // const checkPair = () => {
+  //     const firstCardIcon = document.querySelector('.card.flipped i');
+  //     if (firstCardIcon) {
+  //         const firstIconClass = firstCardIcon.className.split(' ');
+  //         const pair = document.querySelectorAll(`.card.flipped .${firstIconClass.pop()}`);
+  //         if (pair.length == 2) {
+  //             points++;
+  //             showPoints(points);
+  //             document.querySelectorAll(`.card.flipped`).forEach( 
+  //                 card => card.classList.add('found') 
+  //             );
+  //         }
+  //     }
+  // }});
+
+
+
+
+
